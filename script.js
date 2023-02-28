d425b2c4affe503ec84c2f7345cbe755ddf26693
hasbullah-jakariah <114131640+hasbullah-jakariah@users.noreply.github.com>
2023-02-28 08:10:30 -0800

    first commit

--- script.js
+++ script.js
@@ -10,10 +10,8 @@ function loader(element) {
     element.textContent = ''
 
     loadInterval = setInterval(() => {
-        // Update the text content of the loading indicator
         element.textContent += '.';
 
-        // If the loading indicator has reached three dots, reset it
         if (element.textContent === '....') {
             element.textContent = '';
         }
@@ -33,9 +31,6 @@ function typeText(element, text) {
     }, 20)
 }
 
-// generate unique ID for each message div of bot
-// necessary for typing text effect for that specific reply
-// without unique ID, typing text will work on every element
 function generateUniqueId() {
     const timestamp = Date.now();
     const randomNumber = Math.random();
@@ -67,26 +62,26 @@ const handleSubmit = async (e) => {
 
     const data = new FormData(form)
 
-    // user's chatstripe
+    
     chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
 
-    // to clear the textarea input 
+    
     form.reset()
 
-    // bot's chatstripe
+    
     const uniqueId = generateUniqueId()
     chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
 
-    // to focus scroll to the bottom 
+    
     chatContainer.scrollTop = chatContainer.scrollHeight;
 
-    // specific message div 
+   
     const messageDiv = document.getElementById(uniqueId)
 
-    // messageDiv.innerHTML = "..."
+    
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://openai-server.cyclic.app', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
